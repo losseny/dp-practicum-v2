a5dc89440dadd6031033529bed0af87c6fd4a058
losseny <lossenyd46@gmail.com>
2024-09-21 13:20:18 +0200

    initial commit

---
+++ src/main/java/nl/hu/App.java
@@ -0,0 +1,59 @@
+package nl.hu;
+
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.Metamodel;
+import nl.hu.database.config.Connect;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    private static final Connection CONNECTION;
+
+    static {
+        System.out.println("Connecting to database...");
+        try {
+            CONNECTION = Connect.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        testFetchAll();
+    }
+
+    /**
+     * P6. Haal alle (geannoteerde) entiteiten uit de database.
+     */
+    private static void testFetchAll() throws SQLException {
+        PreparedStatement preparedStatement = CONNECTION.prepareStatement("SELECT * FROM reiziger ORDER BY reiziger_id");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.println("Alle reizigers:");
+        while (resultSet.next()) {
+            System.out.printf(
+                    "\t#%d: %s. %s %s (%s)%n",
+                    resultSet.getRow(),
+                    resultSet.getString("voorletters"),
+                    Optional.ofNullable(resultSet.getString("tussenvoegsel")).orElse(""),
+                    resultSet.getString("achternaam"),
+                    resultSet.getDate("geboortedatum")
+            );
+
+        }
+    }
+}
---
+++ src/main/java/nl/hu/database/config/Connect.java
@@ -0,0 +1,24 @@
+package nl.hu.database.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+
+    private static final String DATABASE_URL = "jdbc:postgresql://localhost:15432/student-db-database-v2";
+    private static final String PASSWORD = "admin";
+    private static final String USERNAME = "admin";
+
+    private static Connection CONNECTION;
+
+
+    public static Connection getConnection() throws SQLException {
+        if (CONNECTION == null) {
+            CONNECTION = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        }
+        System.out.println("Connected to database...");
+        System.out.println("Connection: " + CONNECTION);
+        return CONNECTION;
+    }
+}
